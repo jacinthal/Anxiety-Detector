3e9c3518484465be51c6b9727dd308b43019ec5a
jacinthal <85028729+jacinthal@users.noreply.github.com>
2022-05-26 17:25:34 -0400

    Create Shared.h

---
+++ Shared.h
@@ -0,0 +1,7 @@
+//--------------------------------------------------------
+//                Shared/All          /*S*/
+//--------------------------------------------------------
+#include <math.h>
+
+long timer; //used for how often to print and how often to 
+String str;
